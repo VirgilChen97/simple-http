cf50fb80a6b0cc0e63f2758e47a14c7388fffb8a
Yifeng Chen <virgilchen97@gmail.com>
2020-01-16 17:01:26 -0600

    fix

--- http_server.py
+++ http_server.py
@@ -1,12 +1,11 @@
 import socket
 import os
-import re
-import atexit
+
 def http_server(port):
 
     # Configure socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('', 8082))
+    sock.bind(('', 8081))
     sock.listen(100)
 
     # infinite loop
@@ -15,14 +14,17 @@ def http_server(port):
         try:
             conn, addr = sock.accept()
             request = bytes.decode(conn.recv(1024))
-            src = request.split(' ')[1]
+            src = request.split(' ')[1][1:]
 
             print ('Connect by: ', addr)
             print ('Request is:\n', request)
             
             content = ''
-            if os.path.exists(src[1:]):
-                file = open(src[1:], 'r')
+
+            if not src.endswith('.htm') and not src.endswith('.html'):
+                content += "HTTP/1.0 403 Forbidden\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
+            elif os.path.exists(src):
+                file = open(src, 'r')
                 content += "HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
                 content += file.read()
                 file.close()
